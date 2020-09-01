1635636bfe20ce5f9946383b35de3396af876172
Goncalo Pinho <goncalopinho@hotmail.com>
2020-09-01 20:39:28 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for express
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory 
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Goncalo Pinho'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Goncalo Pinho'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Are you looking for help?',
+        title: 'Help',
+        name: 'Goncalo Pinho'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+
+        if (error) {
+            return res.send({ error });
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+    
+            if (error) {
+                return res.send({ error })
+            }
+       
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Goncalo',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Goncalo Pinho',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, cb) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=4d255ec2f2fec622db0657dd2ebf6f22&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        
+        if (error) {
+            cb('Unable to connect to weather service', undefined);
+            return;
+        }
+
+        if (body.error) {
+            cb('Unable to find location', undefined);
+            return;
+        }
+
+        const weatherForecast = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.';
+        cb(undefined, weatherForecast);
+
+    })
+}
+
+module.exports = forecast
